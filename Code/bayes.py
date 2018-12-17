61479f276e0503064b930d41f393ae254b123419
llemes <lamija.lemes@gmail.com>
2018-12-17 20:46:01 +0100

    Added export and import model functionality

--- Code/bayes.py
+++ Code/bayes.py
@@ -6,35 +6,56 @@ import os as os
 from sklearn.metrics import accuracy_score
 from sklearn.naive_bayes import GaussianNB
 from sklearn.naive_bayes import BernoulliNB
+import pickle
 
 os.chdir('..')
+here = os.path.dirname(os.path.abspath(__file__))
+exportGaussianNB = os.path.join(here, '../Model/modelExportGaussianNB.sav')
+exportGaussianNBCustom = os.path.join(here, '../Model/modelExportGaussianNBCustom.sav')
+exportBernoulli = os.path.join(here, '../Model/modelExportBernoulliNB.sav')
+
+loadModels = input('Load models? (Y/N)')
 
 train = pd.read_csv('./DeskriptorTrain.csv').as_matrix()
-outcome = train[:,7]
-train = train[:,0:7]
+outcome = train[:, 7]
+train = train[:, 0:7]
 
 test = pd.read_csv('./DeskriptorTest.csv').as_matrix()
-test=test.reshape(21,7)
+test = test.reshape(21,7)
 
-model = GaussianNB()
-model.fit(train, outcome)
+if (loadModels == 'N') | (loadModels == 'n'):
+	model = GaussianNB()
+	model.fit(train, outcome)
+	pickle.dump(model, open(exportGaussianNB, 'wb'))
+else:
+	model = pickle.load(open(exportGaussianNB, 'rb'))
+
 predicted = model.predict(test)
 
-truth = [1,1,1,1,1,1,1,2,2,2,2,2,2,2,3,3,3,3,3,3,3]
+truth = [1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3]
 
 acc = accuracy_score(predicted, truth)
 print("Accuracy sa accuracy_score za citav model (Gaussian bez priors): {}\n".format(acc))
 
-model = GaussianNB(priors=[0.9, 0.05, 0.05])
-model.fit(train, outcome)
+if (loadModels == 'N') | (loadModels == 'n'):
+	model = GaussianNB(priors=[0.9, 0.05, 0.05])
+	model.fit(train, outcome)
+	pickle.dump(model, open(exportGaussianNBCustom, 'wb'))
+else:
+	model = pickle.load(open(exportGaussianNBCustom, 'rb'))
+
 predicted = model.predict(test)
 
 acc = accuracy_score(predicted, truth)
 print("Accuracy sa accuracy_score za citav model (Gaussian sa priors): {}\n".format(acc))
 
+if (loadModels == 'N') | (loadModels == 'n'):
+	model = BernoulliNB()
+	model.fit(train, outcome)
+	pickle.dump(model, open(exportBernoulli, 'wb'))
+else:
+	model = pickle.load(open(exportBernoulli, 'rb'))
 
-model = BernoulliNB()
-model.fit(train, outcome)
 pred = model.predict(test)
 
 
